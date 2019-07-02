6a452c457e07f58b6b4631e993118c0487daeec7
Иван Поддубный <ip@webpractik.ru>
2019-07-02 13:42:49 +0300

    init

---
+++ examples/simple.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$api = new Kladr\Api('51dfe5d42fb2b43e3300006e', '86a2c2a06f1b2451a87d05512cc2c3edfdf41969');
+
+$query              = new Kladr\Query();
+$query->ContentName = 'Арх';
+$query->ContentType = Kladr\ObjectType::City;
+$query->WithParent  = TRUE;
+$query->Limit       = 2;
+
+$arResult = $api->QueryToArray($query);
+
+print '<pre>';
+var_dump($arResult);
+print '</pre>';
